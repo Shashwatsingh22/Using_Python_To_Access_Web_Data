71ac6da99640210fc83de0849b9c385e68d25453
Shashwat SIngh <55051478+Shashwatsingh22@users.noreply.github.com>
2020-06-27 12:00:10 +0530

    Update extrcting_email_by_use_of_regular_expression.py

--- extrcting_email_by_use_of_regular_expression.py
+++ extrcting_email_by_use_of_regular_expression.py
@@ -6,3 +6,7 @@ for line in lines:
     line = line.rstrip()
     line = re.findall("\S+@\S+",line)
     print(line)
+    
+#We can make it more perfect (Fine Tunning) by the use of Parnthise {i.e ()}
+    line = re.findall("^From (\S+@\S+)",line)
+    print(line)        
