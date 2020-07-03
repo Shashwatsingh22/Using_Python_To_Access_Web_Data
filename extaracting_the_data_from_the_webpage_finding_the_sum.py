014b271246c7f9c2c579638388ab8180cf74029b
Shashwat SIngh <55051478+Shashwatsingh22@users.noreply.github.com>
2020-07-03 20:06:12 +0530

    Update extaracting_the_data_from_the_webpage_finding_the_sum.py

--- extaracting_the_data_from_the_webpage_finding_the_sum.py
+++ extaracting_the_data_from_the_webpage_finding_the_sum.py
@@ -8,7 +8,7 @@ ctx.check_hostname=False
 ctx.verify_mode=ssl.CERT_NONE
 
 url=input("Enter the URL:")
-html=urllib.request.urlopen(url).read()
+html=urllib.request.urlopen(url,context=ctx).read()
 soup=BeautifulSoup(html,'html.parser')
 
 summ=0
